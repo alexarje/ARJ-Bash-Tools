4cdcbf937e12f9140a2a9df82a16f6d8ce997570
Alexander Refsum Jensenius <arjensenius@gmail.com>
2019-01-12 21:11:57 +0100

    Adding more functionality

--- scripts/mxf2mp4.sh
+++ scripts/mxf2mp4.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
-for i in *.MXF;
-do
-    name=`echo $i | cut -d'.' -f1`;
-#    ffmpeg -i "$i" -c:v libx264 -vf yadif "$name_conv.mp4";
+# Script for converting MXF video files to differen to other file formats.
+
+for i in *.MXF; do
+ if [ -e "$i" ]; then
+   file=`basename "$i" .MXF`
+   # MP4 file with default settings + deinterlacing
+   ffmpeg -i "$i" -c:v libx264 -vf yadif "$file.mp4"
+   # AVI file with MJPEG compression that works with QTM
+#   ffmpeg -i "$i" -c:v mjpeg -q:v 3 -acodec pcm_s16le -ar 44100 "$name_conv.avi";
+   # Uncompressed audio file
+#   ffmpeg -i "$i" -acodec pcm_s16le "$name_conv.wav";
+   # Create thumbnail from the first image in the video file
+   ffmpeg -i "$i" -vf "thumbnail" -frames:v 1 "$file.png"
+ fi
 done
